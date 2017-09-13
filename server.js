aa61addf0114095ed6b6a847796c686ee5555070
petogriac <peter.griac@gmail.com>
2017-09-13 10:45:45 +0200

    Changed port

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ var db;
 MongoClient.connect(dbConfig.url, (err, database) => {
   if (err) return console.log(err)
   require('./app/routes')(app, database);
-  app.listen(3000, () => {
+  app.set('port', (process.env.PORT || 3000));
+  app.listen(app.get('port'), () => {
     console.log('listening on 3000')
   })
 })
