13b20fd8830e006738a03a9f8c6f15ffced21e29
petogriac <peter.griac@gmail.com>
2017-09-13 13:26:48 +0200

    solving delete problems

--- app/routes/todo_routes.js
+++ app/routes/todo_routes.js
@@ -4,6 +4,7 @@ module.exports = function(app, db) {
     app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
       next();
     });
     // Creating a todo
